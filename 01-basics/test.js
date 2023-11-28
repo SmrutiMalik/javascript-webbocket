f44d615c524aafd59938563b868063c58717eeba
SmrutiMalik <152277721+SmrutiMalik@users.noreply.github.com>
2023-11-28 11:59:41 +0000

    test file added

---
+++ 01-basics/test.js
@@ -0,0 +1,11 @@
+console.log("hellow world");
+/*javascript is a scripting language used to devloped web & mobile application efficiently.
+we used javascript in client side as well as server side programming language.
+javascript was devloped to write logic functionality to our code to perform everywhere.
+javascript was devloped in 1997 and the 1st version was ES1 and later on it was ES2,ES3.....
+now we used ES6 version after 2015 ES6-ECHAMA SCRIPT6(VANILLA JAVASCRIPT)
+
+
+
+
+*/
